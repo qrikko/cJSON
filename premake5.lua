0c2dbba089901e1f590b3d403ef9866f9571996a
Qrikko <qrikko@gmail.com>
2022-02-03 22:13:37 +0100

    updated premake

--- premake5.lua
+++ premake5.lua
@@ -1,22 +1,21 @@
 project "cjson"
-    kind "staticLib"
-    language "C"
-    cdialect "C11"
-    staticruntime "on"
+	kind "staticLib"
+	language "C"
+	cdialect "C11"
+	staticruntime "on"
+	toolset "clang"
 
-    targetdir (engine_root .. "/lib/" .. outputdir)
-    objdir (engine_root .. "/bin-int/" .. outputdir .. "/%{prj.name}")
+	targetdir (engine_root .. "/lib/" .. outputdir)
+	objdir (engine_root .. "/bin-int/" .. outputdir .. "/%{prj.name}")
 
-    files {
-        "cJSON.h",
-        "cJSON.c",
-    }
+	files {
+		"cJSON.h",
+		"cJSON.c",
+	}
 
-    filter "configurations:Debug"
-        runtime "Debug"
-        symbols "on"
-    filter "configurations:Release"
-        runtime "Release"
-        optimize "on"
-
-
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
